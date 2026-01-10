08ba58604b5833d573db7711ac361796641ffaac
myrrahman <m.y.ibadurrahman127@gmail.com>
2026-01-10 19:22:10 +0700

    add modal button

--- src/js/modules/modal.ts
+++ src/js/modules/modal.ts
@@ -1,7 +1,14 @@
 import { getFilteredFiles } from './gallery.js';
 import { log, getElementOrThrow, MediaFile } from './utils.js';
 
+let modalIndex: number;
+let nextBtn: HTMLElement | null;
+let prevBtn: HTMLElement | null;
+
 export function openModal(index: number): void {
+  console.log(modalIndex);
+  modalIndex = index;
+  console.log(modalIndex);
   const modal = getElementOrThrow('modal');
   const filteredFiles = getFilteredFiles();
 
@@ -23,7 +30,7 @@ export function openModal(index: number): void {
     const video = document.createElement('video');
     video.src = file.url;
     video.className = 'modal-media';
-    video.control = true;
+    video.controls = true;
     mediaElement = video;
   }
 
@@ -52,12 +59,43 @@ export function closeModal() {
   document.body.style.overflow = 'auto';
 }
 
+export function nextMedia() {
+  const filteredFiles = getFilteredFiles();
+
+  if (modalIndex < filteredFiles.length - 1) {
+    openModal(modalIndex + 1)
+  } else {
+    nextBtn.classList.remove('hover:text-slate-50');
+    log('last file', 'warn');
+  }
+}
+
+export function prevMedia() {
+  const filteredFiles = getFilteredFiles();
+
+  if (modalIndex > 0) {
+    openModal(modalIndex - 1);
+  } else {
+    prevBtn.classList.remove('hover:text-slate-50');
+    log('first file', 'warn');
+  }
+}
+
 export function setupModalButtons(): void {
   try {
     const closeBtn = document.querySelector('.modal-close') as HTMLElement | null;
+    nextBtn = document.querySelector('.modal-nav.next') as HTMLElement | null;
+    prevBtn = document.querySelector('.modal-nav.prev') as HTMLElement | null;
     if (closeBtn) {
       closeBtn.addEventListener('click', closeModal);
     }
+    if (nextBtn) {
+      nextBtn.addEventListener('click', nextMedia);
+    }
+    if (prevBtn) {
+      prevBtn.addEventListener('click', prevMedia);
+    }
+    log('Modal button setup success', 'success');
   } catch (error) {
     const message = error instanceof Error ? error.message : String(error);
     log(`Error setting up modal buttons: ${message}`, 'error');
