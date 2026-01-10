58289e969b579dff4a27bc46391c984ae0f4c0db
myrrahman <m.y.ibadurrahman127@gmail.com>
2026-01-10 17:39:22 +0700

    implemented modal/lightbox (worked while need UI adjustment)

--- src/js/modules/modal.ts
+++ src/js/modules/modal.ts
@@ -10,11 +10,45 @@ export function openModal(index: number): void {
   }
 
   const file = filteredFiles[index];
+  const container = getElementOrThrow('modalMediaContainer');
+  container.innerHTML = '';
+
+  let mediaElement: HTMLElement;
+  if (file.type == 'image') {
+    const img = document.createElement('img');
+    img.src = file.url;
+    img.className = 'modal-media';
+    mediaElement = img;
+  } else {
+    const video = document.createElement('video');
+    video.src = file.url;
+    video.className = 'modal-media';
+    video.control = true;
+    mediaElement = video;
+  }
+
+  container.appendChild(mediaElement);
+
+  const fileName = getElementOrThrow('modalFileName');
+  const fileIndex = getElementOrThrow('modalFileIndex');
+  const fileTotal = getElementOrThrow('modalFileTotal');
+
+  fileName.textContent = file.name;
+  fileIndex.textContent = (index + 1).toString();
+  fileTotal.textContent = filteredFiles.length.toString();
+
+  modal.classList.remove('hidden');
+
+  // supaya user gak bisa scroll
+  // kenapa? Karena seluruh element body di luar viewport adalah 'overflow'
+  document.body.style.overflow = 'hidden';
+  log('Modal opened', 'success');
 }
 
 export function closeModal() {
   const modal = getElementOrThrow('modal');
   modal.classList.add('hidden');
+  document.body.style.overflow = 'auto';
 }
 
 export function setupModalButtons(): void {
