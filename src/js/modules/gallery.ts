b19179599af85e0c3335b0ba79f547094204587a
myrrahman <m.y.ibadurrahman127@gmail.com>
2026-01-14 01:37:29 +0700

    made animated_image category to serve static image

--- src/js/modules/gallery.ts
+++ src/js/modules/gallery.ts
@@ -25,12 +25,18 @@ function createMediaItems(file: MediaFile, index: number): HTMLElement {
   item.className = 'media-item';
 
   let thumbnail: HTMLElement;
-  if (file.type == 'image' || file.type == 'animated_image') {
+  if (file.type == 'image') {
     const img = document.createElement('img');
     img.src = file.url;
     img.alt = file.name;
     img.className = 'media-thumbnail';
     thumbnail = img;
+  } else if (file.type == 'animated_image') {
+    const anim_img = document.createElement('img');
+    anim_img.src = file.thumbnail_url;
+    anim_img.alt = file.name;
+    anim_img.className = 'media-thumbnail';
+    thumbnail = anim_img;
   } else {
     const video = document.createElement('video');
     video.className = 'media-thumbnail';
